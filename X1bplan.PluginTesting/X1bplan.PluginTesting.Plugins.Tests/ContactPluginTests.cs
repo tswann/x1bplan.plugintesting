5f0cb454287371a071958b31b1d08cb0b4fed2da
Thomas Swann <ts.swann@gmail.com>
2013-02-14 17:24:49 +0100

    Adding unit test and plugin sample code.

---
+++ X1bplan.PluginTesting/X1bplan.PluginTesting.Plugins.Tests/ContactPluginTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Moq;
+using NUnit.Framework;
+using X1bplan.PluginTesting.X1bplan.PluginTesting.Plugins;
+
+namespace X1bplan.PluginTesting.Plugins.Tests
+{
+    [TestFixture]
+    public class ContactPluginTests : Helpers.PluginTestBase
+    {
+        private const string OPEN_TASKS_EXCEPTION = "This Contact has open Tasks associated with it.";
+
+        [SetUp]
+        public void Setup()
+        {
+            this.PipelineContextMock.Setup(p => p.UserId).Returns(Guid.NewGuid);
+            this.PipelineContextMock.Setup(p => p.PrimaryEntityName).Returns("contact");
+            this.PipelineContextMock.Setup(p => p.Stage).Returns(STAGE_PREVALIDATION);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfOpenTaskExists()
+        {
+            Entity contact = new Entity()
+            {
+                LogicalName = "contact",
+                Id = Guid.NewGuid()
+            };
+
+            ParameterCollection parameters = new ParameterCollection();
+            parameters["Target"] = contact.ToEntityReference();
+            this.PipelineContextMock.Setup(p => p.InputParameters).Returns(parameters);
+            this.PipelineContextMock.Setup(p => p.MessageName).Returns("Delete");
+            EntityCollection tasks = new EntityCollection() { EntityName = "task", Entities = { GetRelatedTask(contact.Id) } };
+            this.OrganizationServiceMock.Setup(s => s.RetrieveMultiple(It.IsAny<QueryExpression>())).Returns(tasks);
+
+            PreValidateContactDelete plugin = new PreValidateContactDelete();
+            var exception = Assert.Throws<InvalidPluginExecutionException>(() => plugin.Execute(ServiceProviderMock.Object));
+            Assert.That(exception.Message, Is.StringContaining(OPEN_TASKS_EXCEPTION));
+        }
+
+        private Entity GetRelatedTask(Guid guid)
+        {
+            Entity task = new Entity("task") { Id = Guid.NewGuid() };
+            task.Attributes.Add("regardingobjectid", guid);
+            task.Attributes.Add("statecode", 0);
+            return task;
+        }
+    }
+}
---
+++ X1bplan.PluginTesting/X1bplan.PluginTesting.Plugins.Tests/Helpers/PluginTestBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Moq;
+
+namespace X1bplan.PluginTesting.Plugins.Tests.Helpers
+{
+    public abstract class PluginTestBase
+    {
+        #region Constants
+        protected const int STAGE_PREVALIDATION = 10;
+        protected const int STAGE_PREOPERATION = 20;
+        protected const int STAGE_POSTOPERATION = 40;
+        #endregion
+
+        #region Properties
+        protected Mock<IServiceProvider> ServiceProviderMock { get; set; }
+        protected Mock<IOrganizationService> OrganizationServiceMock { get; set; }
+        protected Mock<IOrganizationServiceFactory> OrgServiceFactoryMock { get; set; }
+        protected Mock<IPluginExecutionContext> PipelineContextMock { get; set; }
+        #endregion
+
+        public PluginTestBase()
+        {
+            ServiceProviderMock = new Mock<IServiceProvider>();
+            OrgServiceFactoryMock = new Mock<IOrganizationServiceFactory>();
+            OrganizationServiceMock = new Mock<IOrganizationService>(MockBehavior.Strict);
+            PipelineContextMock = new Mock<IPluginExecutionContext>(MockBehavior.Strict);
+
+            InitializeDefaults();
+        }
+
+        private void InitializeDefaults()
+        {
+            ServiceProviderMock.Setup(s => s.GetService(typeof(IPluginExecutionContext))).Returns(PipelineContextMock.Object);
+            ServiceProviderMock.Setup(s => s.GetService(typeof(IOrganizationServiceFactory))).Returns(OrgServiceFactoryMock.Object);
+            OrgServiceFactoryMock.Setup(f => f.CreateOrganizationService(It.IsAny<Guid>())).Returns(OrganizationServiceMock.Object);
+        }
+    }
+}
